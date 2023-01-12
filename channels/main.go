cfc9806ed94a481b00e875aeb6f47787b47a1518
Gabriel Gaspar <gabrielgaspar447@gmail.com>
2023-01-12 18:13:32 -0300

    channel stuff

--- channels/main.go
+++ channels/main.go
@@ -2,21 +2,32 @@ package main
 
 import (
 	"fmt"
+	"sync"
+	"time"
 )
 
 func main() {
 	c := make(chan int)
+	wg := sync.WaitGroup{}
+
+	wg.Add(2)
 
 	go func() {
-		for i := 1; i < 1000; i++ {
+		for i := 1; i <= 1000; i++ {
 			c <- i
 			fmt.Printf("Add %d to channel\n", i)
+			time.Sleep(time.Millisecond * 1)
 		}
-
 		close(c)
+		wg.Done()
 	}()
 
-	for n := range c {
-		fmt.Printf("Read %d from channel\n", n)
-	}
+	go func() {
+		for n := range c {
+			fmt.Printf("Read %d from channel\n", n)
+		}
+		wg.Done()
+	}()
+
+	wg.Wait()
 }
