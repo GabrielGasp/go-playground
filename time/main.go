994eb26674ea5f834b6da7473d5f4e7d735c05ad
Gabriel Gaspar <gabrielgaspar447@gmail.com>
2023-08-12 19:58:40 -0300

    commit

--- time/main.go
+++ time/main.go
@@ -6,5 +6,11 @@ import (
 )
 
 func main() {
-	fmt.Println(time.Now().UTC())
+	time1 := time.Date(2023, 8, 12, 15, 0, 0, 0, time.UTC)
+	time2 := time.Date(2023, 8, 12, 15, 0, 0, 0, time.Local)
+
+	fmt.Println(time1)
+	fmt.Println(time2)
+	fmt.Println(time1.Before(time2))
+	fmt.Println(time2.Sub(time1))
 }
